ad18c45a07ab21c1eae615d1e79d8f4410ee28bd
João Luis <joaoluis@trf2.jus.br>
2025-02-12 12:44:46 -0300

    Incluindo dominios permitidos que podem ser carregados com mix content

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,19 @@ const nextConfig = {
     experimental: {
         missingSuspenseWithCSRBailout: false,
     },
+    async headers() {
+        return [
+          {
+            source: '/(.*)', // Aplica para todas as rotas
+            headers: [
+              {
+                key: 'Content-Security-Policy',
+                value: "frame-ancestors 'self' https://sei-apresentacao.trf2.jus.br;" // Permite iframe apenas de https://sei-apresentacao.trf2.jus.br
+              }
+            ]
+          }
+        ]
+      }
 };
 
-export default nextConfig;
+export default nextConfig
