7aaae7490956eb324dbf6a4270309ea0c8ad00ac
Thiago Carlos da Cruz <conhcz@trf.net>
2025-02-26 15:43:31 -0300

    Ajusta as variáveis

--- app/conclusao-de-deslocamento/page.tsx
+++ app/conclusao-de-deslocamento/page.tsx
@@ -34,59 +34,59 @@ function ConclusaoDeslocamento(Frm: FormHelper) {
         setStartDate(date);
     };
 
-function handleBeneficiarioChange(beneficiario: any, Frm: FormHelper) {
-    if (beneficiario && beneficiario.sigla) {
-        Frm.set('funcaoBeneficiario', beneficiario.funcao || '');
-        Frm.set('cargoBeneficiario', beneficiario.cargo || '');
+function handleProponenteChange(proponente: any, nameCargoProponente: any, Frm: FormHelper) {
+    if (proponente) {
+        Frm.set('funcaoProponente', proponente.funcao || '');
+        Frm.set(nameCargoProponente, proponente.cargo || '');
     }
 }
 
-function handleProponenteChange(proponente: any, Frm: FormHelper) {
-    if (proponente) {
-        Frm.set('funcaoProponente', proponente.funcao || '');
-        Frm.set('cargoProponente', proponente.cargo || '');
+function handleBeneficiarioChange(beneficiario: any, nameCargoBeneficiario: any, Frm: FormHelper) {
+    if (beneficiario && beneficiario.sigla) {
+        Frm.set('funcaoBeneficiario', beneficiario.funcao || '');
+        Frm.set(nameCargoBeneficiario, beneficiario.cargo || '');
     }
 }
 
 function handleRetornoAOrigemChange(event: React.ChangeEvent<HTMLInputElement>) {
-    Frm.set('RetornoAOrigem', event.target.value);
+    Frm.set('retorno_a_origem', event.target.value);
 }
 
 return (
     <div>
         <strong>CONCLUSÃO DE DESLOCAMENTO</strong>
         <p><strong>Dados para o relatório de deslocamentos</strong></p>
-        <Frm.TextArea label="Código da Solicitação de Deslocamento:" name="codigo" width={12} />
-        <Frm.TextArea label="Data da Solicitação de Deslocamento:" name="dataDeslocamento" width={12} />
+        <Frm.TextArea label="Código da Solicitação de Deslocamento:" name="solicitacao_codigo" width={12} />
+        <Frm.TextArea label="Data da Solicitação de Deslocamento:" name="solicitacao_data" width={12} />
 
         <div style={{ marginTop: '20px' }}></div> {/* Add spacing */}
 
         <h2>Dados do Proponente</h2>
-        <Pessoa Frm={Frm} name="proponente" label1="Matrícula" label2="Nome" onChange={(proponente) => handleProponenteChange(proponente, Frm)} />
-        <Frm.TextArea label="Cargo:" name="cargoProponente" width={12} />
+        <Pessoa Frm={Frm} name="proponente" label1="Matrícula" label2="Nome" onChange={(proponente) => handleProponenteChange(proponente, 'prop_cargo_funcao', Frm)} />
+        <Frm.TextArea label="Cargo:" name="prop_cargo_funcao" width={12} />
 
         <div style={{ marginTop: '20px' }}></div> {/* Add spacing */}
 
         <h2>Dados do Beneficiário</h2>
-        <Frm.Select label="Tipo de Beneficiário" name="tipoBeneficiario" options={tipoBeneficiarioOptions} width={12} />
-        <Pessoa Frm={Frm} name="beneficiario" label1="Matrícula" label2="Nome" onChange={(pessoa) => handleBeneficiarioChange(pessoa, Frm)} />
-        <Frm.TextArea label="Cargo:" name="cargoBeneficiario" width={12} />
-        <Frm.TextArea label="Finalidade:" name="Finalidade" width={12} />
-        <Frm.Select label='Tipo de Viagem' name='tipoViagem' options={tipoDeslocamentoOptions} width={12} />
-        <Frm.TextArea label="Itinerário:" name="Itinerario" width={12} />
+        <Frm.Select label="Tipo de Beneficiário" name="benef_tipo" options={tipoBeneficiarioOptions} width={12} />
+        <Pessoa Frm={Frm} name="beneficiario" label1="Matrícula" label2="Nome" onChange={(pessoa) => handleBeneficiarioChange(pessoa, 'benef_cargo_funcao', Frm)} />
+        <Frm.TextArea label="Cargo:" name="benef_cargo_funcao" width={12} />
+        <Frm.TextArea label="Finalidade:" name="finalidade" width={12} />
+        <Frm.Select label='Tipo de Viagem' name='tipo_viagem' options={tipoDeslocamentoOptions} width={12} />
+        <Frm.TextArea label="Itinerário:" name="itinerario" width={12} />
         <div style={{ marginTop: '20px' }}>
                 <label>Retorno à Origem:</label>
                 <FormCheck
                     type="radio"
                     label="Sim"
-                    name="RetornoAOrigem"
+                    name="retorno_a_origem"
                     value="Sim"
                     onChange={handleRetornoAOrigemChange}
                 />
                 <FormCheck
                     type="radio"
                     label="Não"
-                    name="RetornoAOrigem"
+                    name="retorno_a_origem"
                     value="Não"
                     onChange={handleRetornoAOrigemChange}
                 />
@@ -108,13 +108,13 @@ return (
         <h2>Cálculo das Diárias</h2>
 
         <div className='row'>
-            <Frm.MoneyInput label="Valor Bruto das Diárias:" name="ValorBrutoDasDiarias" width={4} />
-            <Frm.MoneyInput label="Adicional de Deslocamento:" name="AdicionalDeDeslocamento" width={4} />
-            <Frm.MoneyInput label="Desconto de Auxílio Alimentação:" name="DescontoDeAuxilioAlimentacao" width={4} />
-            <Frm.MoneyInput label="Desconto de Auxílio Transporte:" name="DescontoDeAuxilioTransporte" width={4} />
-            <Frm.MoneyInput label="Desconto de Teto:" name="DescontoDeTeto" width={4} />
-            <Frm.MoneyInput label="Valor Líquido das Diárias:" name="ValorLiquidoDasDiarias" width={4} />
-            <Frm.MoneyInput label="Valor Total das Passagens:" name="ValorTotalDasPassagens" width={4} />
+            <Frm.MoneyInput label="Valor Bruto das Diárias:" name="diarias_bruto" width={4} />
+            <Frm.MoneyInput label="Adicional de Deslocamento:" name="adic_desloc" width={4} />
+            <Frm.MoneyInput label="Desconto de Auxílio Alimentação:" name="desc_a_alim" width={4} />
+            <Frm.MoneyInput label="Desconto de Auxílio Transporte:" name="desc_a_transp" width={4} />
+            <Frm.MoneyInput label="Desconto de Teto:" name="desc_teto" width={4} />
+            <Frm.MoneyInput label="Valor Líquido das Diárias:" name="diarias_liquido" width={4} />
+            <Frm.MoneyInput label="Valor Total das Passagens:" name="passagens_liquido" width={4} />
         </div>
     </div>
 );
@@ -124,19 +124,19 @@ function document(data: any) {
     const Frm = new FormHelper();
     Frm.update(data);
     const {
-        codigo,
-        dataDeslocamento,
-        cargoProponente,
-        Finalidade,
-        Itinerario,
-        RetornoAOrigem,
-        ValorBrutoDasDiarias,
-        AdicionalDeDeslocamento,
-        DescontoDeAuxilioAlimentacao,
-        DescontoDeAuxilioTransporte,
-        DescontoDeTeto,
-        ValorLiquidoDasDiarias,
-        ValorTotalDasPassagens,
+        solicitacao_codigo,
+        solicitacao_data,
+        prop_cargo_funcao,
+        finalidade,
+        itinerario,
+        retorno_a_origem,
+        diarias_bruto,
+        adic_desloc,
+        desc_a_alim,
+        desc_a_transp,
+        desc_teto,
+        diarias_liquido,
+        passagens_liquido,
     } = Frm.data;
 
 
@@ -163,34 +163,34 @@ return (
         <p><strong>CONCLUSÃO DE DESLOCAMENTO</strong></p>
         <strong>Dados para o relatório de deslocamentos</strong><br></br>
 
-        <label>Código da Solicitação de Deslocamento: </label>{' '}{codigo || "Não informado"}<br></br>
-        <label>Data da Solicitação de Deslocamento: </label>{' '}{dataDeslocamento || "Não informado"}<br></br>
+        <label>Código da Solicitação de Deslocamento: </label>{' '}{solicitacao_codigo || "Não informado"}<br></br>
+        <label>Data da Solicitação de Deslocamento: </label>{' '}{solicitacao_data || "Não informado"}<br></br>
 
         {/* DADOS DO PROPONENTE */}
         <label>Proponente:</label>{' '}{data.proponente?.descricao || 'Não informado'}<br></br>
-        <label>Cargo do Proponente:</label>{' '}{cargoProponente || "Não informado"}<br></br>
+        <label>Cargo do Proponente:</label>{' '}{prop_cargo_funcao || "Não informado"}<br></br>
 
         {/* DADOS DO BENEFICIÁRIO */}
-        <label>Tipo de Beneficiário:</label> {getOptionName(tipoBeneficiarioOptions, data.tipoBeneficiario)} <br></br>
+        <label>Tipo de Beneficiário:</label> {getOptionName(tipoBeneficiarioOptions, data.benef_tipo)} <br></br>
         <label>Beneficiário:</label> {data.beneficiario?.descricao || 'Não informado'}<br></br>
-        <label>Cargo do Beneficiário:</label> {data.cargoBeneficiario || 'Não informado'}<br></br>
-        <label>Finalidade:</label>{' '}{Finalidade || "Não informado"}<br></br>
-        <label>Tipo de Viagem:</label> {getOptionName(tipoDeslocamentoOptions, data.tipoViagem)} <br></br>
-        <label>Itinerário:</label>{' '}{Itinerario || "Não informado"}<br></br>
-        <label>Retorno à Origem:</label>{' '}{RetornoAOrigem || "Não informado"}<br></br>
+        <label>Cargo do Beneficiário:</label> {data.benef_cargo_funcao || 'Não informado'}<br></br>
+        <label>Finalidade:</label>{' '}{finalidade || "Não informado"}<br></br>
+        <label>Tipo de Viagem:</label> {getOptionName(tipoDeslocamentoOptions, data.tipo_viagem)} <br></br>
+        <label>Itinerário:</label>{' '}{itinerario || "Não informado"}<br></br>
+        <label>Retorno à Origem:</label>{' '}{retorno_a_origem || "Não informado"}<br></br>
 
         {/* DADOS DO DESLOCAMENTO */}
         <label>Período:</label> De {formatDateToBrazilian(data.periodoDe)} até {formatDateToBrazilian(data.periodoAte)}<br></br>
         <label>Meio de Transporte:</label>{' '}{getOptionName(meioTransporteOptions, data.meioTransporte)}<br></br>
 
         {/* CÁLCULO DAS DIÁRIAS */}
-        <label>Valor Bruto das Diárias:</label>{' '}{formatCurrency(ValorBrutoDasDiarias) || "Não informado"}<br></br>
-        <label>Adicional de Deslocamento:</label>{' '}{formatCurrency(AdicionalDeDeslocamento) || "Não informado"}<br></br>
-        <label>Desconto de Auxílio Alimentação:</label>{' '}{formatCurrency(DescontoDeAuxilioAlimentacao) || "Não informado"}<br></br>
-        <label>Desconto de Auxílio Transporte:</label>{' '}{formatCurrency(DescontoDeAuxilioTransporte) || "Não informado"}<br></br>
-        <label>Desconto de Teto:</label>{' '}{formatCurrency(DescontoDeTeto) || "Não informado"}<br></br>
-        <label>Valor Líquido das Diárias:</label>{' '}{formatCurrency(ValorLiquidoDasDiarias) || "Não informado"}<br></br>
-        <label>Valor Total das Passagens:</label>{' '}{formatCurrency(ValorTotalDasPassagens) || "Não informado"}<br></br>
+        <label>Valor Bruto das Diárias:</label>{' '}{formatCurrency(diarias_bruto) || "Não informado"}<br></br>
+        <label>Adicional de Deslocamento:</label>{' '}{formatCurrency(adic_desloc) || "Não informado"}<br></br>
+        <label>Desconto de Auxílio Alimentação:</label>{' '}{formatCurrency(desc_a_alim) || "Não informado"}<br></br>
+        <label>Desconto de Auxílio Transporte:</label>{' '}{formatCurrency(desc_a_transp) || "Não informado"}<br></br>
+        <label>Desconto de Teto:</label>{' '}{formatCurrency(desc_teto) || "Não informado"}<br></br>
+        <label>Valor Líquido das Diárias:</label>{' '}{formatCurrency(diarias_liquido) || "Não informado"}<br></br>
+        <label>Valor Total das Passagens:</label>{' '}{formatCurrency(passagens_liquido) || "Não informado"}<br></br>
     </div>
 );
 }
