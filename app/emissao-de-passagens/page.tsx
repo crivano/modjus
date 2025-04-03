b544e19da4d68614b3dbc9fbc0d5b28e8243f78e
ThiagoHouse <thiagocrz11@gmail.com>
2025-04-03 16:08:48 -0300

    Formata valor de saída do formulário de emissão de passagens

--- app/emissao-de-passagens/page.tsx
+++ app/emissao-de-passagens/page.tsx
@@ -5,93 +5,92 @@ import { FormHelper } from "@/libs/form-support";
 import Model from "@/libs/model";
 import styles from './emissao-de-passagens.module.css'; // Import the CSS module
 
-function EmissaoPassagens(Frm: FormHelper) {
-    const [startDate, setStartDate] = useState<Date | null>(new Date());
-    const handleDateChange = (date: Date | null) => {
-        setStartDate(date);
-    };
-
-    return (
-        <div className={styles.scrollableContainer}>
-            <h3>EMISSÃO DE PASSAGENS</h3>
-            <strong>Passagens</strong>
-            <Frm.DynamicListDadosEmbarque label="Dados do Embarque" name="dadosEmbarque" />
-            <p><strong>Reserva</strong></p>
-            <Frm.Input label="Número do RPA:" name="rpa_passagens" />
-            <Frm.Space px="20px" />
-            <p><strong>Custos</strong></p>
-            <Frm.MoneyInput label="Valor Total das Passagens:" name="valor_passagens" width={6} />
-        </div>
-    );
-}
-
-function document(data: any) {
+export default function EmissaoPassagens() {
     const Frm = new FormHelper();
-    Frm.update(data);
-    const {
-        rpa_passagens,
-        valor_passagens,
-        dadosEmbarque,
-    } = Frm.data;
 
-    const formatDateToBrazilian = (date: string) => {
-        if (!date) return 'Não informado';
-        return date;
-    };
+    function interview(Frm: FormHelper) {
+        const [startDate, setStartDate] = useState<Date | null>(new Date());
+        const handleDateChange = (date: Date | null) => {
+            setStartDate(date);
+        };
 
-    const formatCurrency = (value: number | string | undefined) => {
-        if (value === undefined) return 'Não informado';
-        if (typeof value === 'string') {
-            value = parseFloat(value);
-            value /= 100;
-        }
-        return value.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
-    };
+        return (
+            <div className={styles.scrollableContainer}>
+                <h3>EMISSÃO DE PASSAGENS</h3>
+                <strong>Passagens</strong>
+                <Frm.DynamicListDadosEmbarque label="Dados do Embarque" name="dadosEmbarque" />
+                <p><strong>Reserva</strong></p>
+                <Frm.Input label="Número do RPA:" name="rpa_passagens" />
+                <Frm.Space px="20px" />
+                <p><strong>Custos</strong></p>
+                <Frm.MoneyInputFloat label="Valor Total das Passagens:" name="valor_passagens" width={6} />
+            </div>
+        );
+    }
 
-    return (
-        <div className='scrollableContainer'>
-            {/* <div style={{ textAlign: 'center', marginBottom: '0.3em', marginTop: '1em', fontWeight: 'bold', fontSize: '120%' }}>
+    function document(data: any) {
+        Frm.update(data);
+        const {
+            rpa_passagens,
+            valor_passagens,
+            valor_pass,
+            dadosEmbarque,
+        } = Frm.data;
+
+        Frm.set('valor_pass', parseFloat(valor_passagens).toFixed(2));
+
+        // const formatDateToBrazilian = (date: string) => {
+        //     if (!date) return 'Não informado';
+        //     return date;
+        // };
+
+        // const formatFloatValue = (value: number): string => {
+        //     return value?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
+        // }
+
+        return (
+            <div className='scrollableContainer'>
+                {/* <div style={{ textAlign: 'center', marginBottom: '0.3em', marginTop: '1em', fontWeight: 'bold', fontSize: '120%' }}>
                 EMISSÃO DE PASSAGENS
             </div> */}
 
-            <span style={{ fontWeight: 'bold' }}>Passagens</span>
+                <span style={{ fontWeight: 'bold' }}>Passagens</span>
 
-            <div style={{ marginTop: '1rem', marginBottom: '1rem', width: '100%' }}>
-                <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid black', backgroundColor: 'white' }}>
-                    <thead>
-                        <tr style={{ textAlign: 'center', backgroundColor: 'lightgray' }}>
-                            <th style={{ border: '1px solid black' }}>Data de Embarque</th>
-                            <th style={{ border: '1px solid black' }}>Trecho</th>
-                            <th style={{ border: '1px solid black' }}>Empresa</th>
-                            <th style={{ border: '1px solid black' }}>Vôo/Linha</th>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {dadosEmbarque?.map((dadosEmbarque: any, index: number) => (
-                            <tr key={index} style={{ textAlign: 'center' }}>
-                                <td style={{ border: '1px solid black' }}>{dadosEmbarque.data_de_embarque}</td>
-                                <td style={{ border: '1px solid black' }}>{dadosEmbarque.trecho}</td>
-                                <td style={{ border: '1px solid black' }}>{dadosEmbarque.empresa}</td>
-                                <td style={{ border: '1px solid black' }}>{dadosEmbarque.vooLinha}</td>
+                <div style={{ marginTop: '1rem', marginBottom: '1rem', width: '100%' }}>
+                    <table style={{ width: '100%', borderCollapse: 'collapse', border: '1px solid black', backgroundColor: 'white' }}>
+                        <thead>
+                            <tr style={{ textAlign: 'center', backgroundColor: 'lightgray' }}>
+                                <th style={{ border: '1px solid black' }}>Data de Embarque</th>
+                                <th style={{ border: '1px solid black' }}>Trecho</th>
+                                <th style={{ border: '1px solid black' }}>Empresa</th>
+                                <th style={{ border: '1px solid black' }}>Vôo/Linha</th>
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
+                        </thead>
+                        <tbody>
+                            {dadosEmbarque?.map((dadosEmbarque: any, index: number) => (
+                                <tr key={index} style={{ textAlign: 'center' }}>
+                                    <td style={{ border: '1px solid black' }}>{dadosEmbarque.data_de_embarque}</td>
+                                    <td style={{ border: '1px solid black' }}>{dadosEmbarque.trecho}</td>
+                                    <td style={{ border: '1px solid black' }}>{dadosEmbarque.empresa}</td>
+                                    <td style={{ border: '1px solid black' }}>{dadosEmbarque.vooLinha}</td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+                
+                <div>
+                    <strong>Reserva</strong>
+                    <p>Número do RPA: <span style={{ color: 'blue' }}>{data.rpa_passagens || 'Não informado'}</span></p>
+                </div>
+                <div>
+                    <strong>Custos</strong>
+                    <p>Valor Total das Passagens: <span style={{ color: 'blue' }}>{ parseFloat(data.valor_passagens || '0').toFixed(2)}</span></p>      
+                </div>
             </div>
+        );
+    }
+    
 
-            <div>
-                <strong>Reserva</strong>
-                <p>Número do RPA: <span style={{ color: 'blue' }}>{rpa_passagens || 'Não informado'}</span></p>
-            </div>
-
-            <div>
-                <strong>Custos</strong>
-                <p>Valor Total das Passagens: <span style={{ color: 'blue' }}>{formatCurrency(valor_passagens) || 'Não informado'}</span></p>
-            </div>
-        </div>
-    );
-}
-
-export default function MeetingMemory() {
-    return Model(EmissaoPassagens, document, { saveButton: true, pdfButton: false, pdfFileName: 'EmissaoPassagens' });
+    return Model(interview, document, { saveButton: true, pdfButton: false, pdfFileName: 'EmissaoPassagens' });
 }
