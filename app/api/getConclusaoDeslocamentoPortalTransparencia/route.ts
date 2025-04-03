37860de56a4973e766162b2e1ca536dccadee019
victor leal <victorhugooliveiraleal@gmail.com>
2025-04-03 16:07:24 -0300

    Adiciona opções de meio de transporte e ajusta lógica de busca na API para exportação de dados em CSV

--- app/api/getConclusaoDeslocamentoPortalTransparencia/route.ts
+++ app/api/getConclusaoDeslocamentoPortalTransparencia/route.ts
@@ -37,6 +37,15 @@ export async function GET(req: NextRequest) {
             { id: '3', name: 'Colaborador Eventual' }
         ];
 
+        const meioTransporteOptions = [
+            { id: '', name: '' },
+            { id: '1', name: 'Aéreo' },
+            { id: '2', name: 'Rodoviário' },
+            { id: '3', name: 'Hidroviário' },
+            { id: '4', name: 'Veículo Próprio' },
+            { id: '5', name: 'Sem Passagens' }
+          ]
+
         const data = (response.data as any[]).map((item: any) => ({
             numero_conclusao: item.numero_documento,
             data_assinatura_conclusao: "00/00/0000", // TODO ajustar com a data de assinatura do documento
@@ -44,14 +53,14 @@ export async function GET(req: NextRequest) {
             solicitacao_de_deslocamento_data: item.modjusData.data_solicitacao, // TODO data de assinatura do documento de deslocamento
             prop_cargo_funcao: item.modjusData.funcaoProponente,
             beneficiario: item.modjusData.pessoa.descricao,
-            beneficiario_tipo: tipoBeneficiarioOptions.find(option => option.id === item.modjusData.tipoBeneficiario)?.name, // Adjust as needed
+            beneficiario_tipo: tipoBeneficiarioOptions.find(option => option.id === item.modjusData.tipoBeneficiario)?.name || "Outro",
             benef_cargo_funcao: item.modjusData.cargoBeneficiario,
             periodo: `${item.modjusData.periodoDe} a ${item.modjusData.periodoAte}`,
             data_inicio: item.modjusData.periodoDe,
             data_fim: item.modjusData.periodoAte,
             itinerario: item.modjusData.origemDestino,
             tipo_viagem: item.modjusData.tipoDeslocamento === "2" ? "Internacional" : "Nacional",
-            meio_de_transporte: item.modjusData.meioTransporte === "1" ? "Aéreo" : "Outro", // Adjust as needed
+            meio_de_transporte: meioTransporteOptions.find(option => option.id === item.modjusData.meioTransporte)?.name || "Outro",
             diarias_bruto: item.modjusData.valorBrutoDiarias || "0.00",
             adic_desloc: item.modjusData.valorAdicionalDeslocamento || "0.00",
             desc_aux_alim: item.modjusData.valorDescontoAlimentacao || "0.00",
@@ -59,18 +68,26 @@ export async function GET(req: NextRequest) {
             subtotal_diarias_bruto: item.modjusData.totalDeDescontoDeTeto || "0.00",
             desc_teto: item.modjusData.totalDeDescontoDeTeto || "0.00",
             diarias_liquido: item.modjusData.valorLiquidoDiarias || "0.00",
-            passagens_liquido: "1255.47", // Adjust as needed
+            passagens_liquido: item.modjusData.valor_passagem || "0.00", // Adjust as needed
             finalidade: item.modjusData.finalidade
         }));
 
         const csv = parse(data);
 
-        return new NextResponse(csv, {
+         return new NextResponse(csv, {
             headers: {
                 'Content-Type': 'text/csv',
                 'Content-Disposition': 'attachment; filename="conclusao_deslocamento.csv"'
             }
-        });
+        }); 
+
+        /* return NextResponse.json(data, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Content-Disposition': 'attachment; filename="conclusao_deslocamento.json"'
+            }
+        }); */
+
     } catch (error) {
         console.error('Erro na API:', error);
         return NextResponse.json({ error: 'Erro ao buscar os dados' }, { status: 500 });
