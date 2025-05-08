f52fadfe0f2f5c97c4ffdb7dadb564a63333c96a
ThiagoHouse <thiagocrz11@gmail.com>
2025-05-08 12:01:09 -0300

    Utiliza o acordion do bootstrap no interview

--- app/atualiza-valores-diarias/page.tsx
+++ app/atualiza-valores-diarias/page.tsx
@@ -6,7 +6,6 @@ import { FormHelper } from "@/libs/form-support"
 export default function AtualizaValoresDiarias() {
 
   const faixaOptions = [
-    { id: '', name: '' },
     { id: '1', name: 'Membro do Conselho' },
     { id: '2', name: 'Desembargador Federal' },
     { id: '3', name: 'Juiz Federal de 1º Grau/Juiz Federal Substituto' },
@@ -22,23 +21,91 @@ export default function AtualizaValoresDiarias() {
     return value?.toLocaleString('pt-BR', { style: 'currency', currency: 'BRL' });
   }
 
+  function colapse(Frm: FormHelper, faixa: string) {
+    // Extrair a primeira palavra (antes do espaço)
+    const prefixo = faixa
+      .split(" ")[0]
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, ""); // Remove os acentos
+
+    return (
+      <div className="container mt-3">
+        <div className="accordion" id={`${prefixo}_accordionExample`}>
+          <div className="card">
+            <div className="card-header" id={`${prefixo}_headingOne`}>
+              <h5 className="mb-0 text-center">
+                <button
+                  className="btn btn-link"
+                  type="button"
+                  data-bs-toggle="collapse"
+                  data-bs-target={`#collapseOne_${prefixo}`}
+                  aria-expanded="true"
+                  aria-controls="collapseOne"
+                >
+                  {faixa}
+                </button>
+              </h5>
+            </div>
+            <div
+              id={`collapseOne_${prefixo}`}
+              className="collapse show"
+              aria-labelledby={`${prefixo}_headingOne`}
+              data-bs-parent={`#${prefixo}accordionExample`}
+            >
+              <div className="card-body">
+                <div className="row">
+                  <div className="col">
+                    <Frm.MoneyInputFloat
+                      label="Diária Exterior:"
+                      name={`${prefixo}_diaria_exterior`}
+                      width={6}
+                    />
+                  </div>
+                  <div className="col">
+                    <Frm.MoneyInputFloat
+                      label="Diária Nacional:"
+                      name={`${prefixo}_diaria_nacional`}
+                      width={6}
+                    />
+                  </div>
+
+                  <div className="col">
+                    <Frm.MoneyInputFloat
+                      label="Meia Diária:"
+                      name={`${prefixo}_meia_diaria`}
+                      width={6}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+
+
   function Interview(Frm: FormHelper) {
 
     return (
       <div className="scrollableContainer">
         <div>
-          <Frm.Input label="Informe abaixo o número dos documentos que alteram valores e regras para o cálculo de diárias: " name="numero_documentos_diarias" width={6} />
-          <Frm.Input label="Número da Portaria do CJF: " name="numero_portaria_cjf" width={6} />
-          <Frm.Input label="Número da Portaria do TRF2: " name="numero_portaria_trf2" width={6} />
-
-          <h3>Tabela de Valores: </h3>
-          {/* <Frm.Input label="Cargo/Função:" name="cargo_funcao" width={6} /> */}
-          <Frm.Select label="Cargo/Função:" name="cargo_funcao" options={faixaOptions} width={6} />
-          <Frm.MoneyInputFloat label="Diária Exterior:" name="diaria_exterior" width={6} />
-          <Frm.MoneyInputFloat label="Diária Nacional:" name="diaria_nacional" width={6} />
-          <Frm.MoneyInputFloat label="Meia Diária:" name="meia_diaria" width={6} />
-
-          <h3>Outros valores: </h3>
+          <Frm.Input label="Informe abaixo o número dos documentos que alteram valores e regras para o cálculo de diárias: " name="numero_documentos_diarias" width={12} />
+          <div className="row">
+            <Frm.Input label="Número da Portaria do CJF: " name="numero_portaria_cjf" width={6} />
+            <Frm.Input label="Número da Portaria do TRF2: " name="numero_portaria_trf2" width={6} />
+          </div>
+          <p></p>
+          <h4>Tabela de Valores: </h4>
+          {
+            faixaOptions.map((option) => (
+              colapse(Frm, option.name)
+            ))
+          }
+          <p></p>
+          <h4>Outros valores: </h4>
 
           <Frm.MoneyInputFloat label="Valor do Teto Diária Nacional: " name="valor_teto_diaria_nacional" width={6} />
           <Frm.MoneyInputFloat label="Valor do Teto Meia Diária Nacional: " name="valor_teto_meia_diaria_nacional" width={6} />
@@ -53,8 +120,8 @@ export default function AtualizaValoresDiarias() {
     return (
       <div className="scrollableContainer">
         <p><strong>Número dos documentos que alteram valores e regras para o cálculo de diárias:</strong> {data.numero_documentos_diarias || 'Não informado'}</p>
-        <p><strong>Número da Portaria do CJF:</strong> {data.numero_portaria_cjf ||'Não informado'}</p>
-        <p><strong>Número da Portaria do TRF2:</strong> {data.numero_portaria_trf2 ||'Não informado'}</p>
+        <p><strong>Número da Portaria do CJF:</strong> {data.numero_portaria_cjf || 'Não informado'}</p>
+        <p><strong>Número da Portaria do TRF2:</strong> {data.numero_portaria_trf2 || 'Não informado'}</p>
 
         <h3>Tabela de Valores: </h3>
 
@@ -69,13 +136,47 @@ export default function AtualizaValoresDiarias() {
           </thead>
           <tbody>
             <tr style={{ textAlign: 'center' }}>
-              <td style={{ border: '1px solid black' }}>{getOptionName(faixaOptions, data.cargo_funcao) || 'Não Informado'}</td>
-              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.diaria_exterior) || 'Não Informado'}</td>
-              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.diaria_nacional) || 'Não Informado'}</td>
-              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.meia_diaria) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{getOptionName(faixaOptions, '1') || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.membro_diaria_exterior) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.membro_diaria_nacional) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.membro_meia_diaria) || 'Não Informado'}</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr style={{ textAlign: 'center' }}>
+              <td style={{ border: '1px solid black' }}>{getOptionName(faixaOptions, '2') || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.desembargador_diaria_exterior) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.desembargador_diaria_nacional) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.desembargador_meia_diaria) || 'Não Informado'}</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr style={{ textAlign: 'center' }}>
+              <td style={{ border: '1px solid black' }}>{getOptionName(faixaOptions, '3') || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.juiz_diaria_exterior) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.juiz_diaria_nacional) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.juiz_meia_diaria) || 'Não Informado'}</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr style={{ textAlign: 'center' }}>
+              <td style={{ border: '1px solid black' }}>{getOptionName(faixaOptions, '4') || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.analista_diaria_exterior) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.analista_diaria_nacional) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.analista_meia_diaria) || 'Não Informado'}</td>
+            </tr>
+          </tbody>
+          <tbody>
+            <tr style={{ textAlign: 'center' }}>
+              <td style={{ border: '1px solid black' }}>{getOptionName(faixaOptions, '5') || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.tecnico_diaria_exterior) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.tecnico_diaria_nacional) || 'Não Informado'}</td>
+              <td style={{ border: '1px solid black' }}>{formatFloatValue(data.tecnico_meia_diaria) || 'Não Informado'}</td>
             </tr>
           </tbody>
         </table>
+
+        <p></p>
 
         <h3>Outros valores: </h3>
         <p><strong>Valor do Teto Diária Nacional:</strong> {formatFloatValue(data.valor_teto_diaria_nacional) || 'Não informado'}</p>
